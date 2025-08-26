d34700dfd9cfa9725ed8675a3de5c1bea325130e
DavidGomezOv <david182400@gmail.com>
2025-08-25 18:09:21 -0600

    Fixed connection issues

--- src/db.js
+++ src/db.js
@@ -7,4 +7,8 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
